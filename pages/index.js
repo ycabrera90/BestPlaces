c4b71c04253b56e9cef9c8e1fd42e642c171966d
yosniel <yosniel.ch@gmail.com>
2022-10-31 13:57:57 -0300

    the MeetupList was added to the project

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import MeetupList from "../components/MeetupList/MeetupList";
 
-const DUMMY_MEETUPS = [
+const DUMMY_PLACES = [
   {
     id: "p1",
     title: "A First Meetup",
     image:
       "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",
     address: "Empire State Building, West 34th Street, New York, NY, USA",
-    description: "New York City is like no other city in the world, and one that must be experienced to be fully appreciated",
+    description:
+      "New York City is like no other city in the world, and one that must be experienced to be fully appreciated",
   },
   {
     id: "p2",
@@ -14,12 +16,13 @@ const DUMMY_MEETUPS = [
     image:
       "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",
     address: "Golden Gate Bridge, Golden Gate Bridge, San Francisco, CA, USA",
-    description: "West Coast city is the perfect getaway destination for couples, singles, or families",
+    description:
+      "West Coast city is the perfect getaway destination for couples, singles, or families",
   },
 ];
 
 function HomePage() {
-  return <></>;
+  return <MeetupList meetups={DUMMY_PLACES} />;
 }
 
 export default HomePage;
