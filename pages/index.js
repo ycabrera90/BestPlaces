7667ae27715fa175c576e886e1bbcd271ed0a229
yosniel <yosniel.ch@gmail.com>
2022-10-31 13:49:37 -0300

    DUMMY_PLACES was added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-// our-domain.com/
+
+const DUMMY_MEETUPS = [
+  {
+    id: "p1",
+    title: "A First Meetup",
+    image:
+      "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",
+    address: "Empire State Building, West 34th Street, New York, NY, USA",
+    description: "New York City is like no other city in the world, and one that must be experienced to be fully appreciated",
+  },
+  {
+    id: "p2",
+    title: "Golden Gate Bridge",
+    image:
+      "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",
+    address: "Golden Gate Bridge, Golden Gate Bridge, San Francisco, CA, USA",
+    description: "West Coast city is the perfect getaway destination for couples, singles, or families",
+  },
+];
 
 function HomePage() {
-  return <h1>The Home Page</h1>
+  return <></>;
 }
 
 export default HomePage;
