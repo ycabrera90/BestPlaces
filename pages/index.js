c137768e35adac08164f6d46f74307fd46b4e720
yosniel <yosniel.ch@gmail.com>
2022-10-31 16:43:18 -0300

    some comment were deleted from the homePage

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,6 @@ import { MongoClient } from "mongodb";
 import PlacesList from "../components/PlacesList/PlacesList";
 import MainLayout from "../components/UI/MainLayout/MainLayout";
 
-// const DUMMY_PLACES = [
-//   {
-//     id: "p1",
-//     title: "New York City",
-//     image:
-//       "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",
-//     address: "Empire State Building, West 34th Street, New York, NY, USA",
-//     description:
-//       "New York City is like no other city in the world, and one that must be experienced to be fully appreciated",
-//   },
-//   {
-//     id: "p2",
-//     title: "Golden Gate Bridge",
-//     image:
-//       "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",
-//     address: "Golden Gate Bridge, Golden Gate Bridge, San Francisco, CA, USA",
-//     description:
-//       "West Coast city is the perfect getaway destination for couples, singles, or families",
-//   },
-// ];
-
 function HomePage({ places }) {
   return (
     <MainLayout>
