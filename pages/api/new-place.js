f9979159b4075b43cc9640bb4d70cd35fb2b07a3
yosniel <yosniel.ch@gmail.com>
2022-10-31 16:12:06 -0300

    the endpoint new-place was created in backend

--- pages/api/new-place.js
+++ pages/api/new-place.js
@@ -1,6 +1,26 @@
+import { MongoClient } from "mongodb";
+
 async function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(201).json({ message: 'Place inserted!' });
+  console.log("hereeeeee");
+  console.log(process.env.DB_PASSWORD);
+
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pbuk80v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const placesCollection = db.collection("places");
+
+    const result = await placesCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Places inserted!" });
   }
 }
 
