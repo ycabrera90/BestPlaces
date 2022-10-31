1bad0e9c263d66be06e4d8f90bc16197d96b356b
yosniel <yosniel.ch@gmail.com>
2022-10-31 16:37:56 -0300

    the places were obtained from Mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
+import { MongoClient } from "mongodb";
+
 import PlacesList from "../components/PlacesList/PlacesList";
 import MainLayout from "../components/UI/MainLayout/MainLayout";
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "New York City",
-    image:
-      "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",
-    address: "Empire State Building, West 34th Street, New York, NY, USA",
-    description:
-      "New York City is like no other city in the world, and one that must be experienced to be fully appreciated",
-  },
-  {
-    id: "p2",
-    title: "Golden Gate Bridge",
-    image:
-      "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",
-    address: "Golden Gate Bridge, Golden Gate Bridge, San Francisco, CA, USA",
-    description:
-      "West Coast city is the perfect getaway destination for couples, singles, or families",
-  },
-];
+// const DUMMY_PLACES = [
+//   {
+//     id: "p1",
+//     title: "New York City",
+//     image:
+//       "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",
+//     address: "Empire State Building, West 34th Street, New York, NY, USA",
+//     description:
+//       "New York City is like no other city in the world, and one that must be experienced to be fully appreciated",
+//   },
+//   {
+//     id: "p2",
+//     title: "Golden Gate Bridge",
+//     image:
+//       "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",
+//     address: "Golden Gate Bridge, Golden Gate Bridge, San Francisco, CA, USA",
+//     description:
+//       "West Coast city is the perfect getaway destination for couples, singles, or families",
+//   },
+// ];
 
 function HomePage({ places }) {
   return (
@@ -31,9 +33,23 @@ function HomePage({ places }) {
 }
 
 export async function getStaticProps() {
+  const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pbuk80v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  const client = await MongoClient.connect(dbUrl);
+  const db = client.db();
+  const placesCollection = db.collection("places");
+  const datas = await placesCollection.find().toArray();
+
+  const places = datas.map((item) => ({
+    title: item.title,
+    address: item.address,
+    image: item.image,
+    id: item._id.toString(),    // <--- this is because _id is an object
+  }));
+
+  client.close();
   return {
     props: {
-      places: DUMMY_PLACES,
+      places,
     },
   };
 }
