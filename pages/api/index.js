adcd4c2158e6e9ee76f27f6eb2953bc24bd09d34
yosniel <yosniel.ch@gmail.com>
2022-10-31 15:15:00 -0300

    the root endpoint of backend was coded

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json({
+    name: "Yosniel",
+    company: "EIP-Studios",
+    message: "welcome to my server",
+    linkedin: "https://linkedin.com/in/ingenieríacubana",
+    github: "https://github.com/ycabrera90",
+  });
+};
