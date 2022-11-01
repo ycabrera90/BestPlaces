dfa0ef01a7028ac48b5975057e336079ed8b6b9c
yosniel <yosniel.ch@gmail.com>
2022-11-01 15:55:42 -0300

    some general adjustment were made in the styles

--- components/PlacesList/PlaceItem/PlaceItem.js
+++ components/PlacesList/PlaceItem/PlaceItem.js
@@ -12,18 +12,16 @@ function PlaceItem(props) {
 
   return (
     <li className={classes.item}>
-      <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
+      {/* <div className={classes.image}> */}
+      <img src={props.image} alt={props.title} />
+      {/* </div> */}
+      {/* <div className={classes.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
+        </div> */}
+      {/* <div className={classes.actions}>
           <button onClick={showDetailsHandler}>Show Details</button>
-        </div>
-      </Card>
+        </div> */}
     </li>
   );
 }
--- components/PlacesList/PlacesList.js
+++ components/PlacesList/PlacesList.js
@@ -1,19 +1,32 @@
-import PlaceItem from './PlaceItem/PlaceItem';
-import classes from './PlacesList.module.css';
+import PlaceItem from "./PlaceItem/PlaceItem";
+import classes from "./PlacesList.module.css";
 
 function PlacesList(props) {
   return (
-    <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
-        <PlaceItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-        />
-      ))}
-    </ul>
+    <div>
+      <ul className={classes.list}>
+        {props.meetups.map((meetup) => (
+          <PlaceItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+          />
+        ))}
+      </ul>
+      <ul className={classes.list}>
+        {props.meetups.map((meetup) => (
+          <PlaceItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
