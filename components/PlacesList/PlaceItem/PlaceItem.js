30e17f2ca5e61b432747d16e18f0313af7d47b29
yosniel <yosniel.ch@gmail.com>
2022-11-02 14:08:40 -0300

    react-horizontal-scrolling  menu was added

--- components/PlacesList/PlaceItem/PlaceItem.js
+++ components/PlacesList/PlaceItem/PlaceItem.js
@@ -1,15 +1,6 @@
-import Card from "../../UI/Card/Card";
-import { useRouter } from "next/router";
-
 import classes from "./PlaceItem.module.css";
 
 function PlaceItem({ id, image, title }) {
-  const router = useRouter();
-
-  function showDetailsHandler() {
-    router.push("/" + id);
-  }
-
   return (
     <li className={classes.item}>
       <img src={image} alt={title} />
--- components/PlacesList/PlacesList.js
+++ components/PlacesList/PlacesList.js
@@ -1,41 +1,66 @@
 import PlaceItem from "./PlaceItem/PlaceItem";
+
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
 import classes from "./PlacesList.module.css";
 
 function PlacesList({ meetups }) {
+  function onWheel(apiObj, ev) {
+    const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+    if (isThouchpad) {
+      ev.stopPropagation();
+      return;
+    }
+
+    if (ev.deltaY < 0) {
+      apiObj.scrollNext();
+      console.log("scrollNext");
+    } else if (ev.deltaY > 0) {
+      apiObj.scrollPrev();
+      console.log("scrollPrev");
+    }
+  }
+
   return (
     <div className={classes["items-container"]}>
       <ul>
-        {meetups.map((meetup) => (
-          <PlaceItem
-            key={meetup.id}
-            id={meetup.id}
-            image={meetup.image}
-            title={meetup.title}
-            address={meetup.address}
-          />
-        ))}
+        <ScrollMenu onWheel={onWheel}>
+          {meetups.map((meetup) => (
+            <PlaceItem
+              key={meetup.id}
+              id={meetup.id}
+              image={meetup.image}
+              title={meetup.title}
+              address={meetup.address}
+            />
+          ))}
+        </ScrollMenu>
       </ul>
       <ul>
-        {meetups.map((meetup) => (
-          <PlaceItem
-            key={meetup.id}
-            id={meetup.id}
-            image={meetup.image}
-            title={meetup.title}
-            address={meetup.address}
-          />
-        ))}
+        <ScrollMenu onWheel={onWheel}>
+          {meetups.map((meetup) => (
+            <PlaceItem
+              key={meetup.id}
+              id={meetup.id}
+              image={meetup.image}
+              title={meetup.title}
+              address={meetup.address}
+            />
+          ))}
+        </ScrollMenu>
       </ul>
       <ul>
-        {meetups.map((meetup) => (
-          <PlaceItem
-            key={meetup.id}
-            id={meetup.id}
-            image={meetup.image}
-            title={meetup.title}
-            address={meetup.address}
-          />
-        ))}
+        <ScrollMenu onWheel={onWheel}>
+          {meetups.map((meetup) => (
+            <PlaceItem
+              key={meetup.id}
+              id={meetup.id}
+              image={meetup.image}
+              title={meetup.title}
+              address={meetup.address}
+            />
+          ))}
+        </ScrollMenu>
       </ul>
     </div>
   );
