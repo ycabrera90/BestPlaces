f5c19057ba1672239b1ccc135fdf700c204fa91a
yosniel <yosniel.ch@gmail.com>
2022-11-05 14:54:01 -0300

    only the middle item in ScrollItems is highlighted

--- components/ItemsScroll/MainItem/MainItem.js
+++ components/ItemsScroll/MainItem/MainItem.js
@@ -5,10 +5,41 @@ import classes from "./MainItem.module.css";
 
 function MainItem({ id, image, title, onClick }) {
   const visibility = React.useContext(VisibilityContext);
-  const visible = visibility.isItemVisible(id);
+  let visible = false;
   // const visible = id === "6363f4374ffdad97440c77f0";
+  // let visible = false;
+  const visibilityItem = visibility.visibleElements;
 
-  // console.log(id);
+  if (visibilityItem.length === 1) {
+    console.log("only one item");
+    visible = visibility.isItemVisible(id);
+  } else if (visibility.visibleElements.length % 2 !== 0) {
+    console.log("odd number of items");
+    const middleItemIndex = Math.floor(visibility.visibleElements.length / 2);
+    const middleItemId = visibilityItem[middleItemIndex];
+    console.log("middleItemIndex", middleItemIndex);
+    console.log("middleItemId", middleItemId);
+    if(id === middleItemId) {
+      visible = true;
+    }
+  } else {
+    console.log("even number of items");
+  }
+
+  // if (visibility.visibleElements.length % 2 !== 0) {
+  //   console.log("odd");
+  //   const visibilityItem =
+  //     // visibility.visibleElements[
+  //     //   (visibility.visibleElements.length - 1) / 2 + 1
+  //     // ];
+  //     visibility.visibleElements;
+  //   console.log(visibilityItem);
+  //   // if (visibilityItem === id) {
+  //   //   visible = true;
+  //   // }
+  // } else {
+  //   // nada visible
+  // }
 
   return (
     <div
