11f0539ee342ecc3e5c797b86fad2de986c66513
yosniel <yosniel.ch@gmail.com>
2022-11-07 16:01:15 -0300

    some adjustments were made

--- components/ItemsScroll/MainItem/MainItem.js
+++ components/ItemsScroll/MainItem/MainItem.js
@@ -1,50 +1,75 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState, memo } from "react";
 import { VisibilityContext } from "react-horizontal-scrolling-menu";
 
 import useDOM_helper from "../../../hooks/useDOM_helper";
 import classes from "./MainItem.module.css";
 
+let isFirstValidDOM_values = true;
+
 function MainItem({ id, image, title, onClick, dueEvent, isFirtElement }) {
   // <-- dueEvent can be: imgClick, wheel, arrowsClick
-
+  const [highlighted, setHighlighted] = useState(false);
   const visibility = useContext(VisibilityContext);
   const { screen } = useDOM_helper();
 
-  const clickHandler = () => {
-    return onClick(visibility);
-  };
+  const goToItem = () => onClick(visibility);
 
   // auto go to the first not emty element if you open the page on mobile screens
   useEffect(() => {
-    setTimeout(() => {
+    let timeBeforeGo;
+    if (isFirstValidDOM_values) {
       if (isFirtElement && screen.orientation === "portrait") {
-        clickHandler();
-      }
-    }, 500);
-  }, []);
-
-  // auto hightlighting center items when click items or when you are on mobile screens
-  let highlighted = false;
-  if (dueEvent === "imgClick" || screen.orientation === "portrait") {
-    const visibilityItems = visibility.visibleElements; 
-    const visibilityItemsLength = visibilityItems.length;
-    if (visibilityItemsLength === 1) {
-      highlighted = visibility.isItemVisible(id);
-    } else if (visibilityItemsLength % 2 !== 0) {
-      if (id === visibilityItems[Math.floor(visibilityItemsLength / 2)]) {
-        highlighted = true;
+        setTimeout(() => {
+          goToItem();
+          isFirstValidDOM_values = false;
+        }, 500);
       }
     }
-  }
+    return () => clearTimeout(timeBeforeGo);
+  }, [isFirtElement, screen.orientation]);
+
+  // auto go to the the last highlighted element when you change the orientation of your screen
+  useEffect(() => {
+    let timeBeforeGo;
+    if (!isFirstValidDOM_values) {
+      if (highlighted) {
+        timeBeforeGo = setTimeout(() => {
+          goToItem();
+        }, 500);
+      }
+    }
+    return () => clearTimeout(timeBeforeGo);
+  }, [screen.orientation]);
+
+  // auto hightlighting center items when click items or when you are on mobile screens
+  useEffect(() => {
+    if (!isFirstValidDOM_values) {
+      setHighlighted(false);
+      if (dueEvent === "imgClick" || screen.orientation === "portrait") {
+        const visibilityItems = visibility.visibleElements;
+        const visibilityItemsLength = visibilityItems.length;
+        if (visibilityItemsLength === 1) {
+          setHighlighted(visibility.isItemVisible(id));
+        } else if (visibilityItemsLength % 2 !== 0) {
+          setHighlighted(
+            id === visibilityItems[Math.floor(visibilityItemsLength / 2)]
+          );
+        }
+      }
+    }
+  }, [
+    dueEvent,
+    screen.orientation,
+    visibility.visibleElements,
+    visibility.isItemVisible,
+  ]);
 
   return (
     <div
       role="button"
       className={classes["imagen-container"]}
-      onClick={clickHandler}
-      style={{
-        visibility: image ? "visible" : "hidden",
-      }}
+      onClick={() => onClick(visibility)}
+      style={{ visibility: image ? "visible" : "hidden" }}
     >
       <div
         className={`${classes.fog} ${highlighted ? classes.highlighted : ""} `}
@@ -58,4 +83,5 @@ function MainItem({ id, image, title, onClick, dueEvent, isFirtElement }) {
   );
 }
 
+// export default memo(MainItem);
 export default MainItem;
--- hooks/useDOM_helper.jsx
+++ hooks/useDOM_helper.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 const useDOM_helper = () => {
   const [orientation, setOrientation] = useState(null);
   const [size, setSize] = useState(null);
 
-  const getOrientation = () => {
+  const getOrientation = useCallback(() => {
     return window.innerWidth > window.innerHeight ? "landscape" : "portrait";
-  };
+  }, []);
 
-  const getSizes = () => {
+  const getSizes = useCallback(() => {
     return {
       width: window.innerWidth,
       height: window.innerHeight,
     };
-  };
+  }, []);
 
   useEffect(() => {
     setOrientation(getOrientation());
@@ -22,9 +22,15 @@ const useDOM_helper = () => {
     window.addEventListener("resize", () => {
       setOrientation(getOrientation());
     });
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        setOrientation(getOrientation());
+      });
+    }
   }, []);
 
   return { screen: { orientation, size } };
-};
+};;;
 
 export default useDOM_helper;
