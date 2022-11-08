d7e61584ffc7cfad38c9a621e923d8a48b17ab64
yosniel <yosniel.ch@gmail.com>
2022-11-08 13:39:06 -0300

    some comment were deleted

--- components/PlacesList/PlaceItem/PlaceItem.jsx
+++ components/PlacesList/PlaceItem/PlaceItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./PlaceItem.module.css";
 
-function PlaceItem({ id, ulId, image, title }) {
+function PlaceItem({ image, title }) {
   return <img className={classes.img} src={image} alt={title} />;
 }
 
