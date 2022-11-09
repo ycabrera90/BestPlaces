aedf78cfa8bc0d761a134fc524597291b6ed8734
yosniel <yosniel.ch@gmail.com>
2022-11-08 21:40:28 -0300

    the Image of nextJs was applied to BpIcon

--- components/UI/BpIcon/BpIcon.js
+++ components/UI/BpIcon/BpIcon.js
@@ -1,9 +1,21 @@
 import classes from "./BpIcon.module.css";
+import Image from "next/image";
 
 const BpIcon = () => {
   return (
     <i className={classes["bp-icon"]}>
-      <img src="/favicon.ico" alt='Best Place'/>
+      {/* <img src="/favicon.ico" alt='Best Place'/> */}
+      <Image
+        src="/favicon.ico"
+        alt="Best Place"
+        width={50}
+        height={50}
+        style={{
+          width: "2.5rem",
+          height: "2.5rem",
+        }}
+        sizes="2.5rem"
+      />
       <h1>PLACES</h1>
     </i>
   );
