9914e121463a8f884a9585c5c207c8fc2f10ab66
yosniel <yosniel.ch@gmail.com>
2022-11-08 21:21:07 -0300

    the Image of nextJs was applied to PlaceDetail

--- components/PlaceDetail/PlaceDetail.jsx
+++ components/PlaceDetail/PlaceDetail.jsx
@@ -1,15 +1,23 @@
+import Image from 'next/image';
 import classes from './PlaceDetail.module.css';
 
-function PlaceDetail(props) {
+function PlaceDetail({ image, title, address, description }) {
   return (
     <section className={classes.detail}>
-      <img
-        src={props.image}
-        alt={props.title}
+      <Image
+        alt={title}
+        src={image}
+        fill
+        sizes="(max-width: 768px) 100vw,                
+                (max-width: 1200px) 50vw,
+                 33vw"
+        style={{
+          objectFit: "cover",
+        }}
       />
-      <h1>{props.title}</h1>
-      <address>{props.address}</address>
-      <p>{props.description}</p>
+      <h1>{title}</h1>
+      <address>{address}</address>
+      <p>{description}</p>
     </section>
   );
 }
