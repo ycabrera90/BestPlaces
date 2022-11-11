43aba7c85d2acb762d3b6ae5f0c9895a4f9b613b
yosniel <yosniel.ch@gmail.com>
2022-11-11 20:18:34 -0300

    other row of images was added at background when portrait mode

--- components/UI/BackgroundImages/BackgroundImages.jsx
+++ components/UI/BackgroundImages/BackgroundImages.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import useDOM_helper from "../../../hooks/useDOM_helper";
 
 import Slide from "../../Slide/Slide";
 import classes from "./BackgroundImages.module.css";
 
 const BackgroundImages = ({ images }) => {
+  const { screen } = useDOM_helper();
+  console.log(screen);
 
   return (
     <div className={classes["background-container"]}>
       <Slide datas={images} speed={150} />
       <Slide datas={images} speed={100} />
+      {screen.orientation === "portrait" && (
+        <Slide datas={images} speed={100} reverse />
+      )}
       <Slide datas={images} speed={150} reverse />
     </div>
   );
