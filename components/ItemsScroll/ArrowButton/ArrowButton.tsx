2b93417d040620d1f1834853ead35e394a14d539
yosniel <yosniel.ch@gmail.com>
2022-11-18 19:22:04 -0300

    the ArrowButton component was coded in typescript

--- components/ItemsScroll/ArrowButton/ArrowButton.tsx
+++ components/ItemsScroll/ArrowButton/ArrowButton.tsx
@@ -1,14 +1,20 @@
+import { FC, ReactNode } from "react";
 import classes from "./ArrowButton.module.css";
 
 
 
-const ArrowButton = ({ children, disabled, onClick, className }) => {
+const ArrowButton: FC<{
+  children: ReactNode;
+  disabled: boolean;
+  onClick: () => void;
+  className: string;
+}> = ({ children, disabled, onClick, className }) => {
   return (
     <button
       className={`${classes["arrow-button"]} ${className ? className : ""}`}
       disabled={disabled}
       onClick={onClick}
-      style={{opacity: disabled ? "0" : "1",}}
+      style={{ opacity: disabled ? "0" : "1" }}
     >
       {children}
     </button>
