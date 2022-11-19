39a39aac62987dd39af7c4739884a5c5db258062
yosniel <yosniel.ch@gmail.com>
2022-11-19 14:15:34 -0300

    some bug with styles was fixed in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
+import "../styles/globals.css";
 
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
