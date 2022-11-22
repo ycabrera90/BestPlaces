bfa07e630307e6c40a00fc87b246563523f4e35e
yosniel <yosniel.ch@gmail.com>
2022-11-21 22:57:50 -0300

    feat: a template folder was added for componentes

---
+++ components/templates/base/BaseTemplate.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
+
+export default {
+  title: 'templates/BaseTemplate',
+  component: BaseTemplate,
+  argTypes: {},
+} as ComponentMeta<typeof BaseTemplate>;
+
+const Template: ComponentStory<typeof BaseTemplate> = (args) => (
+  <BaseTemplate {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockBaseTemplateProps.base,
+} as IBaseTemplate;
+
+
+
+// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
