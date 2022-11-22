d74f901e599e3918f92a71b29539332f76748264
yosniel <yosniel.ch@gmail.com>
2022-11-21 23:08:15 -0300

    fix: some configurations was added to storyBook for add new stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import * as NextImage from 'next/image';
+
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+
+// Allow Storybook to handle Next's <Image> component
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
